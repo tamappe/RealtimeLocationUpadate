f9b567a23b4c086d0e1a4a648e4ccd70b56f1766
Tamappe <tamapppe@gmail.com>
2023-12-03 00:02:32 +0900

    first commit

---
+++ Sources/RealtimeLocationUpdate/RealtimeLocationUpdate.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct RealtimeLocationUpdate {
+    public var text = "hello, world"
+    public init() {
+        
+    }
+}
