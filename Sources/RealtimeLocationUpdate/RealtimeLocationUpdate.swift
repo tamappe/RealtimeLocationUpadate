f8a7c4d064855248e7c1da27553d7edf6795de2c
Tamappe <tamapppe@gmail.com>
2023-12-03 01:15:05 +0900

    仮の実装

--- Sources/RealtimeLocationUpdate/RealtimeLocationUpdate.swift
+++ Sources/RealtimeLocationUpdate/RealtimeLocationUpdate.swift
@@ -1,9 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public struct RealtimeLocationUpdate {
-    public var text = "hello, world"
+import CoreLocation
+
+public class RealtimeLocationUpdate {
+    public var text = "hello, RealtimeLocationUpdate"
+    
+    public var updates: CLLocationUpdate.Updates
+    public var latestLocation: CLLocation?
+    
     public init() {
+        updates = CLLocationUpdate.liveUpdates()
+    }
+    
+    func checkUserPermission() {
         
     }
+    
+    func getLocation() async throws {
+        for try await update in updates {
+            guard let location = update.location else { return }
+            latestLocation = location
+        }
+    }
 }
+
