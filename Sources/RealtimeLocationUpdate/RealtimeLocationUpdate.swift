ee608c0f51a4492ee571c62e0510f28507d1f363
Tamappe <tamapppe@gmail.com>
2023-12-03 17:11:13 +0900

    liveUpdateを使った位置情報取得でアプリ側を表示させる

--- Sources/RealtimeLocationUpdate/RealtimeLocationUpdate.swift
+++ Sources/RealtimeLocationUpdate/RealtimeLocationUpdate.swift
@@ -3,24 +3,48 @@
 
 import CoreLocation
 
-public class RealtimeLocationUpdate {
+public class RealtimeLocationUpdate: NSObject, ObservableObject {
     public var text = "hello, RealtimeLocationUpdate"
     
     public var updates: CLLocationUpdate.Updates
+   
+    @Published
     public var latestLocation: CLLocation?
+    @Published
+    public var authorizationStatus: CLAuthorizationStatus
     
-    public init() {
+    @Published
+    public var locationHistoryList: [CLLocation] = []
+    
+    private let locationManager: CLLocationManager
+    
+    public override init() {
+        locationManager = CLLocationManager()
         updates = CLLocationUpdate.liveUpdates()
+        authorizationStatus = locationManager.authorizationStatus
+        super.init()
     }
     
-    func checkUserPermission() {
+    public func requestWhenInUseAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
         
     }
     
-    func getLocation() async throws {
+    public func requestAlwaysAuthorization() {
+        locationManager.requestAlwaysAuthorization()
+    }
+
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+    }
+    
+    public func getLocation() async throws {
         for try await update in updates {
             guard let location = update.location else { return }
-            latestLocation = location
+            DispatchQueue.main.async {
+                self.locationHistoryList.append(location)
+                self.latestLocation = location
+            }
         }
     }
 }
