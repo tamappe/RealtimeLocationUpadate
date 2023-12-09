f6aa075df840f8b8cfee5d665edd5af18bb3e926
Tamappe <tamapppe@gmail.com>
2023-12-10 00:37:45 +0900

    強制停止、バックグラウンドモード、isStationaryフラグの追加、停止の実装

--- Sources/RealtimeLocationUpdate/RealtimeLocationUpdate.swift
+++ Sources/RealtimeLocationUpdate/RealtimeLocationUpdate.swift
@@ -4,24 +4,33 @@
 import CoreLocation
 
 public class RealtimeLocationUpdate: NSObject, ObservableObject {
-    public var text = "hello, RealtimeLocationUpdate"
     
     public var updates: CLLocationUpdate.Updates
    
     @Published
     public var latestLocation: CLLocation?
+    public var backgroundActivity: CLBackgroundActivitySession?
     @Published
     public var authorizationStatus: CLAuthorizationStatus
     
     @Published
     public var locationHistoryList: [CLLocation] = []
     
+    // 強制停止用フラグ
+    @Published
+    public var isForceStop: Bool = false
+    // バックグラウンドモード, アプリの権限が「使用中」でもバックグラウンドで取得できる
+    @Published
+    public var isBackgroundMode: Bool
+    public var isStationary: Bool = false
+    
     private let locationManager: CLLocationManager
     
     public override init() {
         locationManager = CLLocationManager()
-        updates = CLLocationUpdate.liveUpdates()
+        updates = CLLocationUpdate.liveUpdates(.fitness)
         authorizationStatus = locationManager.authorizationStatus
+        isBackgroundMode = false
         super.init()
     }
     
@@ -39,12 +48,31 @@ public class RealtimeLocationUpdate: NSObject, ObservableObject {
     }
     
     public func getLocation() async throws {
+        self.backgroundActivity?.invalidate()
+        if isBackgroundMode {
+            self.backgroundActivity = CLBackgroundActivitySession()
+        }
         for try await update in updates {
-            guard let location = update.location else { return }
-            DispatchQueue.main.async {
-                self.locationHistoryList.append(location)
-                self.latestLocation = location
+            isStationary = update.isStationary
+            if !isForceStop {
+                if !update.isStationary {
+                    guard let location = update.location else { return }
+                    DispatchQueue.main.async {
+                        self.locationHistoryList.append(location)
+                        self.latestLocation = location
+                        print("getLocation \(location.coordinate.latitude)")
+                    }
+                } else {
+                    isStationary = true
+                }
             }
+        }
+    }
+    
+    public func stopLocatoin() async throws {
+        self.backgroundActivity?.invalidate()
+        for try await update in updates {
+            break
         }
     }
 }
